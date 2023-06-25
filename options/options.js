e6bba7b3b42f769280efcb832009f2eb058fe21a
Crowdbotics <team@crowdbotics.com>
2023-06-25 17:34:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fancy-tooth-41730",
+  url: "https://fancy-tooth-41730.botics.co",
+  api: "https://fancy-tooth-41730.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
